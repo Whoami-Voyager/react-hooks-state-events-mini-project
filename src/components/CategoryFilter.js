ddcd1dfb889fcf814931c71e2dd12a36f9b45533
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-03-13 15:49:51 -0600

    coming back to this later

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useTransition } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ category }) {
+
+  // const [drain, setCategory] = useState("All");
+  // const handleCategoryClick = (filter) => {
+  //   setCategory(filter);
+  // };
+  // const filteredItems = category.filter((item) => {
+  //   if (drain === "All") {
+  //     return true;
+  //   }
+  //   return item === drain;
+  // });
+
+  const dropDown = category.map((filters, index) => {
+    return <button key={index} onClick={ {}}>{filters}</button>
+  })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {dropDown}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Task() {
+function Task({ text, category, deleteTask }) {
+
+  const [tasks, setTasks] = useState([text]);
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={() => { deleteTask(text) }}>X</button>
     </div>
   );
 }
+
 
 export default Task;
